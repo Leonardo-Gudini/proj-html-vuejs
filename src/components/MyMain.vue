1140c3c84d3cf403ee959bb47bc957af5d31450d
Leonardo Gudini <leonardo.gudini98@gmail.com>
2022-09-06 10:03:10 +0200

    add: popular-dishes

--- src/components/MyMain.vue
+++ src/components/MyMain.vue
@@ -1,18 +1,31 @@
 <template>
   <main>
-
+    <PopDishes/>
+    <MenuCat/>
+    <OrderApp/>
+    <LateNews/>
   </main>
 </template>
 
 <script>
+import PopDishes from './sub-components-main/PopDishes.vue';
+import MenuCat from './sub-components-main/MenuCat.vue';
+import OrderApp from './sub-components-main/OrderApp.vue';
+import LateNews from './sub-components-main/LateNews.vue';
+
 export default {
     name: "MyMain",
   components: {
-    
-  }
+    PopDishes,
+    MenuCat,
+    OrderApp,
+    LateNews
+}
 }
 </script>
 
-<style>
+<style lang="scss">
+
+  @import "@/style/main.scss";
 
 </style>
--- src/components/sub-components-main/PopDishes.vue
+++ src/components/sub-components-main/PopDishes.vue
@@ -1,5 +1,17 @@
 <template>
-  <section></section>
+  <section class="pop-dishes">
+    <h2>Our Most Popular Dishes</h2>
+
+    <ul>
+        <li v-for="(dishe, index) in dishes" :key="index">
+            <img :src="'@/assets/images/' + dishe.img" alt="dishe.title">
+            <h3>{{dishe.title}}</h3>
+            <h4>{{dishe.price}}</h4>
+        </li>
+    </ul>
+
+    <button class="btn-standard">start your order <span><i class="fa-regular fa-circle-right"></i></span> </button>
+  </section>
 </template>
 
 <script>
@@ -7,7 +19,23 @@ export default {
     name: "PopDishes",
     data(){
         return{
-
+            dishes: [
+                {
+                    img: 'skin-on-fries-400x571.jpg',
+                    title: 'Skin On Fries',
+                    price: '$3.00-$6.00'
+                },
+                {
+                    img: 'choco-cookie-frappe-400x571.jpg',
+                    title: 'Chocco Cookie Frappe',
+                    price: '$4.99'
+                },
+                {
+                    img: 'donut-burger-400x571.jpg',
+                    title: 'The Donut Burger',
+                    price: '$6.99'
+                },
+            ]
         }
     }
 
@@ -15,5 +43,5 @@ export default {
 </script>
 
 <style lang="scss">
-
+    @import "@/style/main.scss";
 </style>
